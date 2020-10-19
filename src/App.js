55dbea3711466a2ac75eb35d2d5053bf6fbd0791
jin <devjinius@gmail.com>
2020-10-19 22:48:44 +0900

    axis

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
 import React, { useRef, useEffect, useState } from 'react';
 import './App.css';
-import { line, select, curveBasis, curveCatmullRom } from 'd3';
+import { line, select, curveCatmullRom, axisBottom, axisRight, scaleLinear } from 'd3';
 
 function App() {
-	const [data, setData] = useState([25, 30, 45, 20, 100, 60]);
-	
+	const [data, setData] = useState([25, 30, 45, 20, 100, 60]);	
 	const svgRef = useRef();
+
 	useEffect(() => {
 		const svg = select(svgRef.current);
+		const xScale = scaleLinear().domain([0, data.length - 1]).range([0, 300]);
+		const yScale = scaleLinear().domain([0, 120]).range([150, 0]);
+
 		const drawLine = line()
-			.x((v, i) => i * 50)
-			.y(v => 170 - v)
+			.x((v, i) => xScale(i))
+			.y(yScale)
 			.curve(curveCatmullRom);
 
+		const xAxis = axisBottom(xScale).ticks(data.length);
+
 		svg
-			.selectAll('path')
+			.select('.x-axis')
+			.style('transform', 'translateY(150px)')
+			.call(xAxis);
+
+		const yAxis = axisRight(yScale).ticks(5);
+
+		svg
+			.select('.y-axis')
+			.style('transform', 'translateX(300px)')
+			.call(yAxis);
+
+		yAxis(svg.select('y-axis'));
+
+		svg
+			.selectAll('.line')
 			.data([data])
 			.join('path')
-			.attr('d', v => drawLine(v))
+			.attr('class', 'line')
+			.attr('d', drawLine)
 			.attr('fill', 'none')
 			.attr('stroke', 'red');
 	}, [data]);
 	return <>
-		<svg ref={svgRef}></svg>
+		<svg ref={svgRef}>
+			<g className="x-axis"/>
+			<g className="y-axis"/>
+		</svg>
 		<br/>
 		<button onClick={() => setData(data.map(value => value + 5))}>update</button>
 		<button onClick={() => setData(data.filter(value => value < 40))}>filter</button>
