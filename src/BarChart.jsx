d56eea3841e99452c8176a23ba01c7cc0e1bf190
jin <devjinius@gmail.com>
2020-11-17 16:42:37 +0900

    add reactive box

--- src/BarChart.jsx
+++ src/BarChart.jsx
@@ -1,16 +1,43 @@
-import React, { useRef, useEffect } from 'react';
-import { select, axisBottom, axisRight, scaleLinear, scaleBand, svg } from 'd3';
+import React, { useRef, useEffect, useState } from 'react';
+import { select, axisBottom, axisRight, scaleLinear, scaleBand, svg, entries } from 'd3';
+
+const useResizeObserver = (ref) => {
+    const [dimension, setDimension] = useState(null);
+
+    useEffect(() => {
+        const target = ref.current;
+        const resizeObserver = new ResizeObserver(entries => {
+            entries.forEach((entry) => {
+                setDimension(entry.contentRect);
+            });
+        });
+
+        resizeObserver.observe(target);
+        return () => {
+            resizeObserver.unobserve(target);
+        }
+    }, [ref]);
+
+    return dimension;
+}
 
 export const BarChart = ({ data }) => {
     const svgRef = useRef();
+    const wrapperRef = useRef();
+    const dimension = useResizeObserver(wrapperRef);
 
 	useEffect(() => {
-		const svg = select(svgRef.current);
+        const svg = select(svgRef.current);
+
+        if (!dimension) {
+            return;
+        }
+        
 		const xScale = scaleBand()
 						.domain(data.map((value, index) => index))
-						.range([0, 300])
+						.range([0, dimension.width])
 						.padding(0.5);
-		const yScale = scaleLinear().domain([0, 120]).range([150, 0]);
+		const yScale = scaleLinear().domain([0, 120]).range([dimension.height, 0]);
 
 		const colorScale = scaleLinear()
 		.domain([75, 100, 150])
@@ -21,7 +48,7 @@ export const BarChart = ({ data }) => {
 
 		svg
 			.select('.x-axis')
-			.style('transform', 'translateY(150px)')
+			.style('transform', `translateY(${dimension.height}px)`)
 			.call(xAxis);
 
 		const yAxis = axisRight(yScale);
@@ -39,7 +66,7 @@ export const BarChart = ({ data }) => {
 
 		svg
 			.select('.y-axis')
-			.style('transform', 'translateX(300px)')
+			.style('transform', `translateX(${dimension.width}px)`)
 			.call(yAxis);
 
 		svg
@@ -57,13 +84,15 @@ export const BarChart = ({ data }) => {
 			.transition()
 			.attr("fill", colorScale)
 			.attr('attr', 50)
-			.attr("height", value => 150 - yScale(value));
-    }, [data]);
+            .attr("height", value => 150 - yScale(value));
+    }, [data, dimension]);
     
     return (
-        <svg ref={svgRef} id="bar-chart">
-            <g className="x-axis"/>
-            <g className="y-axis"/>
-        </svg>
+        <div ref={wrapperRef} id="bar-chart-wrapper">
+            <svg ref={svgRef} id="bar-chart">
+                <g className="x-axis"/>
+                <g className="y-axis"/>
+            </svg>
+        </div>
     );
 }
